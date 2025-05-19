4a1d8069dbb001f5cf34f7fd0f107a27d676cb6d
TommyPohl <tadyjetomovo@gmail.com>
2025-05-19 21:21:32 +0200

    products

--- server.py
+++ server.py
@@ -27,6 +27,21 @@ def about():
 def contact():
 	return render_template("contact.html")
 
+@app.route("/calculator/<int:a>/<b>")
+def calculator(a, b):
+	b = int(b)
+	return render_template("calculator.html", result=a+b)
+
+@app.route("/products")
+def products():
+	products = [(1, "Mrkev"), (2, "Jabko"), (3, "Avokado"), (4, "Rajče")]
+	return render_template("products.html", products=products)
+
+@app.route("/product/<int:id>")
+def product(id):
+	magic = id % 2
+	return render_template("product.html", magic=magic)
+
 
 
 
