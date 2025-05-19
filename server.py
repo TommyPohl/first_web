f0cb4c6684c3d8d27fc7233737e9590b7f5f927e
TommyPohl <tadyjetomovo@gmail.com>
2025-05-19 18:54:50 +0200

    templates

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello World"
+    name = "Adam"
+    return render_template("index.html", name=name)
+
+@app.route("/about")
+def about():
+    return "<h1>Hello About</h1>"
 
 
 if __name__ == "__main__":
