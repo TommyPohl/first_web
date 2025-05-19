32137109fe16873685d6bdd8f8ada806fe208122
TommyPohl <tadyjetomovo@gmail.com>
2025-05-19 19:33:49 +0200

    first web

--- server.py
+++ server.py
@@ -1,17 +1,31 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
+
+
+@app.route("/ukazka")
+def showcase():
+	name = "Adam"
+	return render_template("index.html", name=name)
+
+
+@app.route("/about-old")
+def about_old():
+	return f"<h1>Hello About</h1>"
 
 
 @app.route("/")
 def home():
-    name = "Adam"
-    return render_template("index.html", name=name)
+	return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return "<h1>Hello About</h1>"
+	return render_template("about.html")
+
+
+
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True, port=5010)
